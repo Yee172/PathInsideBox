06f81c866495082a9332f806b22ecfdab95ce47c
Qihao Ye <11610812@mail.sustc.edu.cn>
2022-11-27 17:58:55 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,33 +9,49 @@ import { FlyControls } from 'three/addons/controls/FlyControls.js';
 
 let controls_TBC, controls_FC;
 
-let prevTime = performance.now();
-
-let moveForward = false;
-let moveBackward = false;
-let moveLeft = false;
-let moveRight = false;
-
-const velocity = new THREE.Vector3();
-const direction = new THREE.Vector3();
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 const stats = new Stats();
 const gui = new GUI();
 
+const colors =
+{
+    background_color: '#cccccc',
+    box_color: '#ffffff',
+    box_edge_color: '#000000', // not used yet
+    line_color: '#ff3333',
+}
+
 const boxes = [];
+const boxes_profile = [];
 const material_box = new THREE.MeshBasicMaterial(
     {
-        color: 0xffffff,
+        color: new THREE.Color( colors.box_color ),
         transparent: true,
-        opacity: 0.5,
+        opacity: 0.3,
         // wireframe: true
     }
 );
 const lines = [];
-const material_line = new THREE.LineBasicMaterial( { color: 0xff3333 } );
+const lines_profile = [];
+const material_line = new THREE.LineBasicMaterial(
+    {
+        color: new THREE.Color( colors.line_color ),
+        // linewidth: 5,
+    }
+);
+
+const initial_profile =
+{
+    boxes_string: '0.5|0,0,0|gray;0.5|0,0.5,0;0.5|0.5,0,0;0.5|0.5,0.5,0|gray; \
+                   0.25|0,0,0.5|gray;0.25|0,0.25,0.5;0.25|0.25,0,0.5;0.25|0.25,0.25,0.5|gray; \
+                   0.25|0.5,0,0.5|gray;0.25|0.5,0.25,0.5;0.25|0.75,0,0.5;0.25|0.75,0.25,0.5|gray; \
+                   0.25|0,0.5,0.5|gray;0.25|0,0.75,0.5;0.25|0.25,0.5,0.5;0.25|0.25,0.75,0.5|gray; \
+                   0.25|0.5,0.5,0.5|gray;0.25|0.5,0.75,0.5;0.25|0.75,0.5,0.5;0.25|0.75,0.75,0.5|gray',
+    lines_string: '0.25,0.25,0.25|0.25,0.25,-0.25|0.25,0.75,-0.25|0.25,0.75,0.25; \
+                   0.75,0.25,0.25|0.75,0.25,-0.25|0.75,0.75,-0.25|0.75,0.75,0.25',
+}
 
 const params =
 {
@@ -44,7 +60,7 @@ const params =
     rotate_direction: -1,
     rotate_radius: 5,
     rotate_speed: 0.01,
-    rotate_angle: 0
+    rotate_angle: 0,
 };
 
 const buttons =
@@ -55,21 +71,24 @@ const buttons =
     change_rotate_direction: change_rotate_direction,
     add_boxes: add_boxes,
     add_lines: add_lines,
+    export_boxes: export_boxes,
+    export_lines: export_lines,
     delete_last_box: delete_last_box,
-    delete_last_line: delete_last_line
+    delete_last_line: delete_last_line,
+    delete_all_boxes: delete_all_boxes,
+    delete_all_lines: delete_all_lines,
 };
 
 initialization();
-string_to_boxes('0.5|0,0,0;0.5|0,0,0.5;0.5|0,0.5,0;0.5|0,0.5,0.5;0.5|0.5,0,0;0.5|0.5,0,0.5;0.5|0.5,0.5,0;0.5|0.5,0.5,0.5;');
-string_to_lines('0.25,0.25,0.25|0.25,0.25,-0.25|0.25,0.75,-0.25|0.25,0.75,0.25;0.75,0.25,0.25|0.75,0.25,-0.25|0.75,0.75,-0.25|0.75,0.75,0.25');
+string_to_boxes( initial_profile.boxes_string );
+string_to_lines( initial_profile.lines_string );
 animate();
 
 function initialization( )
 {
     // Scene
-    scene.background = new THREE.Color( 0xcccccc );
-    scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
-    // scene.fog = new THREE.Fog(0xFFFFFF, 0.1, 1000);
+    scene.background = new THREE.Color( colors.background_color );
+    scene.fog = new THREE.FogExp2( colors.background_color, 0.002 );
     
     // Camera
     camera_reset();
@@ -86,39 +105,27 @@ function initialization( )
     document.body.appendChild( stats.domElement );
 
     // Objects
-    const worldAxis = new THREE.AxesHelper( 3 );
-    scene.add(worldAxis);
+    const world_axis = new THREE.AxesHelper( 2.5 );
+    scene.add( world_axis );
 
-    const plane = new THREE.GridHelper( 5, 100 );
-    scene.add(plane);
-
-    // Lights
-    const dirLight1 = new THREE.DirectionalLight( 0xffffff );
-    dirLight1.position.set( 1, 1, 1 );
-    scene.add( dirLight1 );
-
-    const dirLight2 = new THREE.DirectionalLight( 0x002288 );
-    dirLight2.position.set( - 1, - 1, - 1 );
-    scene.add( dirLight2 );
-
-    const ambientLight = new THREE.AmbientLight( 0x222222 );
-    scene.add( ambientLight );
+    const plane = new THREE.GridHelper( 4, 64 );
+    scene.add( plane );
 
     // Actions
     window.addEventListener( 'resize', onWindowResize );
     createControls( camera );
 
-    const gui_camera = gui.addFolder('Camera');
+    const gui_camera = gui.addFolder( 'Camera' ).close();
     gui_camera.add( buttons, 'change_camera_mode' ).name( 'Change camera mode' );
     gui_camera.add( buttons, 'camera_reset' ).name( 'Reset' );
     gui_camera.add( buttons, 'camera_axis_align' ).name( 'Axis align' );
     gui_camera.add( camera.position, 'x' ).name( 'x' ).listen();
     gui_camera.add( camera.position, 'z' ).name( 'z' ).listen();
     gui_camera.add( camera.position, 'y' ).name( 'y' ).listen();
-    const gui_rotate = gui.addFolder('Auto rotation');
+    const gui_rotate = gui.addFolder( 'Auto rotation' );
     gui_rotate.add( params, 'auto_rotate' ).name( 'Active' );
     gui_rotate.add( buttons, 'change_rotate_direction' ).name( 'Change direction' );
-    gui_rotate.add( params, 'rotate_radius', 0.5, 100 ).name( 'Radius' ).listen().onChange(
+    gui_rotate.add( params, 'rotate_radius', 0.5, 10 ).name( 'Radius' ).listen().onChange(
         function( value )
         {
             var distance = get_distance_to_axisY();
@@ -127,65 +134,84 @@ function initialization( )
         }
     );
     gui_rotate.add( params, 'rotate_speed', 0.001, 0.01 ).name( 'Speed' );
-    const gui_modify = gui.addFolder('Modification');
-    gui_modify.add( buttons, 'add_boxes' ).name( 'Add boxes' );
-    gui_modify.add( buttons, 'add_lines' ).name( 'Add lines' );
-    gui_modify.add( buttons, 'delete_last_box' ).name( 'Delete last box' );
-    gui_modify.add( buttons, 'delete_last_line' ).name( 'Delete last line' );
+    const gui_modify = gui.addFolder( 'Modification' );
+    const gui_IO = gui_modify.addFolder( 'IO' );
+    gui_IO.add( buttons, 'add_boxes' ).name( 'Add boxes' );
+    gui_IO.add( buttons, 'add_lines' ).name( 'Add lines' );
+    gui_IO.add( buttons, 'export_boxes' ).name( 'Export boxes' );
+    gui_IO.add( buttons, 'export_lines' ).name( 'Export lines' );
+    const gui_deletion = gui_modify.addFolder( 'Deletion' ).close();
+    gui_deletion.add( buttons, 'delete_last_box' ).name( 'Delete last box' );
+    gui_deletion.add( buttons, 'delete_last_line' ).name( 'Delete last line' );
+    gui_deletion.add( buttons, 'delete_all_boxes' ).name( 'Delete all boxes' );
+    gui_deletion.add( buttons, 'delete_all_lines' ).name( 'Delete all lines' );
 
     THREEx.FullScreen.bindKey({ charCode : 'm'.charCodeAt(0) });
 }
 
+function remove_all_spaces( s )
+{
+    return s.replace( /\s+/g, '' );
+}
+
 function string_to_one_box( s )
 {
-    s = s.trim();
-    if ( s.length == 0 ) return;
-
-    const content_list = s.split( '|' );
-    const whd = content_list[ 0 ].trim().split( ',' );
-    var geometry_box, material, width, height, depth;
-    material = material_box;
-
-    if ( whd.length == 1 )
+    try
     {
-        width  = Math.abs( Number( whd[ 0 ] ) );
-        height = width;
-        depth  = width;
-    }
-    else if ( whd.length == 3 )
-    {
-        width  = Math.abs( Number( whd[ 0 ] ) );
-        height = Math.abs( Number( whd[ 1 ] ) );
-        depth  = Math.abs( Number( whd[ 2 ] ) );
-    }
-    else
-    {
-        throw 'Wrong input for size of the box!';
-    }
-    geometry_box = new THREE.BoxGeometry( width, height, depth );
+        s = s.trim();
+        if ( s.length == 0 ) return;
 
-    const position = content_list[ 1 ].trim().split( ',' );
-    if ( position.length != 3 ) throw 'Wrong input for position of the box!';
+        const content_list = s.split( '|' );
+        const whd = content_list[ 0 ].trim().split( ',' );
+        var geometry_box, material, width, height, depth;
+        material = material_box;
 
-    if ( content_list.length > 2 )
-    {
-        material = new THREE.MeshBasicMaterial(
-            {
-                color: Number(content_list[ 2 ]),
-                transparent: true,
-                opacity: 0.5
-            }
+        if ( whd.length == 1 )
+        {
+            width  = Math.abs( Number( whd[ 0 ] ) );
+            height = width;
+            depth  = width;
+        }
+        else if ( whd.length == 3 )
+        {
+            width  = Math.abs( Number( whd[ 0 ] ) );
+            height = Math.abs( Number( whd[ 1 ] ) );
+            depth  = Math.abs( Number( whd[ 2 ] ) );
+        }
+        else
+        {
+            throw 'Wrong input for size of the box!';
+        }
+        geometry_box = new THREE.BoxGeometry( width, height, depth );
+
+        const position = content_list[ 1 ].trim().split( ',' );
+        if ( position.length != 3 ) throw 'Wrong input for position of the box!';
+
+        if ( content_list.length > 2 )
+        {
+            material = new THREE.MeshBasicMaterial(
+                {
+                    color: new THREE.Color( content_list[ 2 ] ),
+                    transparent: true,
+                    opacity: 0.5
+                }
+            );
+        }
+
+        const box = new THREE.Mesh( geometry_box, material );
+        box.position.set(
+            Number( position[ 0 ] ) + width  / 2,
+            Number( position[ 1 ] ) + height / 2,
+            Number( position[ 2 ] ) + depth  / 2
         );
+        boxes.push( box );
+        boxes_profile.push( remove_all_spaces(s) );
+        scene.add( box );
     }
-
-    const box = new THREE.Mesh( geometry_box, material );
-    box.position.set(
-        Number( position[ 0 ] ) + width  / 2,
-        Number( position[ 1 ] ) + height / 2,
-        Number( position[ 2 ] ) + depth  / 2
-    );
-    boxes.push( box );
-    scene.add( box );
+    catch ( error )
+    {
+        console.error( error );
+    }
 }
 
 function string_to_boxes( s )
@@ -198,49 +224,73 @@ function add_boxes( )
     string_to_boxes( prompt( 'Boxes profile [ eg. `3|0,1,2` for box (0,1,2) - (3,4,5) ]:' ) );
 }
 
+function export_boxes( )
+{
+    alert( boxes_profile.join( ';' ) );
+}
+
 function delete_last_box( )
 {
     if ( boxes.length > 0 )
     {
         const last_box = boxes.pop();
-        scene.remove(last_box);
+        boxes_profile.pop();
+        scene.remove( last_box );
+    }
+}
+
+function delete_all_boxes( )
+{
+    while ( boxes.length > 0 )
+    {
+        const last_box = boxes.pop();
+        boxes_profile.pop();
+        scene.remove( last_box );
     }
 }
 
 function string_to_one_line( s )
 {
-    s = s.trim();
-    if ( s.length == 0 ) return;
-
-    const content_list = s.split( '|' );
-    if ( content_list.length < 2 ) throw 'No enough points for forming a line';
-
-    const points = [];
-    var material, color_flag;
-    material = material_line;
-    color_flag = content_list[ content_list.length - 1 ].indexOf( ',' ) == -1;
-
-    for ( let i = 0; i < content_list.length - color_flag; i++ )
+    try
     {
-        const xyz = content_list[ i ].split( ',' );
-        if ( xyz.length != 3 ) throw 'Wrong input for point in line';
-        points.push( new THREE.Vector3(
-            Number( xyz[ 0 ] ),
-            Number( xyz[ 1 ] ),
-            Number( xyz[ 2 ] )
-        ) );
-    }
-    const geometry_line = new THREE.BufferGeometry().setFromPoints( points );
+        s = s.trim();
+        if ( s.length == 0 ) return;
 
-    if ( color_flag )
-    {
-        material = new THREE.LineBasicMaterial(
-            { color: Number( content_list[ content_list.length - 1 ] ) }
-        );
+        const content_list = s.split( '|' );
+        if ( content_list.length < 2 ) throw 'No enough points for forming a line';
+
+        const points = [];
+        var material, color_flag;
+        material = material_line;
+        color_flag = content_list[ content_list.length - 1 ].indexOf( ',' ) == -1;
+
+        for ( let i = 0; i < content_list.length - color_flag; i++ )
+        {
+            const xyz = content_list[ i ].split( ',' );
+            if ( xyz.length != 3 ) throw 'Wrong input for point in line';
+            points.push( new THREE.Vector3(
+                Number( xyz[ 0 ] ),
+                Number( xyz[ 1 ] ),
+                Number( xyz[ 2 ] )
+            ) );
+        }
+        const geometry_line = new THREE.BufferGeometry().setFromPoints( points );
+
+        if ( color_flag )
+        {
+            material = new THREE.LineBasicMaterial(
+                { color: new THREE.Color( content_list[ content_list.length - 1 ] ) }
+            );
+        }
+        const line = new THREE.Line( geometry_line, material );
+        lines.push( line );
+        lines_profile.push( remove_all_spaces( s ) );
+        scene.add( line );
     }
-    const line = new THREE.Line( geometry_line, material );
-    lines.push( line );
-    scene.add( line );
+    catch ( error )
+    {
+        console.error( error );
+    }
 }
 
 function string_to_lines( s )
@@ -253,18 +303,34 @@ function add_lines( )
     string_to_lines( prompt( 'Lines profile [ eg. `0,1,2|1,2,3` for line (0,1,2) - (1,2,3) ]:' ) );
 }
 
+function export_lines( )
+{
+    alert( lines_profile.join( ';' ) );
+}
+
 function delete_last_line( )
 {
     if ( lines.length > 0 )
     {
         const last_line = lines.pop();
-        scene.remove(last_line);
+        lines_profile.pop();
+        scene.remove( last_line );
+    }
+}
+
+function delete_all_lines( )
+{
+    while ( lines.length > 0 )
+    {
+        const last_line = lines.pop();
+        lines_profile.pop();
+        scene.remove( last_line );
     }
 }
 
 function createControls( camera )
 {
-    if ( params.camera_mode === 0)
+    if ( params.camera_mode === 0 )
     {
         // TrackballControls
         controls_TBC = new TrackballControls( camera, renderer.domElement );
@@ -284,7 +350,7 @@ function createControls( camera )
         // FlyControls
         controls_FC = new FlyControls( camera, renderer.domElement );
 
-        controls_FC.movementSpeed = 10;
+        controls_FC.movementSpeed = 1;
         controls_FC.domElement = renderer.domElement;
         controls_FC.rollSpeed = Math.PI / 8;
         controls_FC.autoForward = false;
@@ -330,7 +396,7 @@ function camera_axis_align( )
 
 function camera_reset( )
 {
-    camera.position.set( 0, 0.5, 2 );
+    camera.position.set( 0, 1, 2 );
     camera.lookAt( 0, 0, 0 );
     camera_axis_align();
 }
